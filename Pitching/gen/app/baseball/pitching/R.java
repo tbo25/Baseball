040a445ff9ffac58070680dbc9a6f7458b1a78d3
Curtis Thibault <curtis.thibault@gmail.com>
2011-11-28 16:57:40 -0600

    Added basic PitchCounter object to track an at-bat

--- Pitching/gen/app/baseball/pitching/R.java
+++ Pitching/gen/app/baseball/pitching/R.java
@@ -15,13 +15,14 @@ public final class R {
     }
     public static final class id {
         public static final int ball=0x7f050001;
-        public static final int column1=0x7f050005;
-        public static final int column2=0x7f050006;
+        public static final int column1=0x7f050006;
+        public static final int column2=0x7f050007;
         public static final int foulBall=0x7f050002;
         public static final int hit=0x7f050003;
-        public static final int outputList=0x7f050004;
+        public static final int outputList=0x7f050005;
+        public static final int pitchCounter=0x7f050004;
         public static final int strike=0x7f050000;
-        public static final int strikeCount=0x7f050007;
+        public static final int strikeCount=0x7f050008;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- Pitching/src/app/baseball/pitching/Pitching.java
+++ Pitching/src/app/baseball/pitching/Pitching.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import app.baseball.pitching.ArrayAdapters.PitchArrayAdapter;
 import app.baseball.pitching.Models.Interfaces.ICoordinate;
 import app.baseball.pitching.Models.Interfaces.IPitch;
 import app.baseball.pitching.Models.Pitch;
+import app.baseball.pitching.Models.PitchCounter;
 import app.baseball.pitching.Models.PitchLocation;
 
 import java.util.ArrayList;
@@ -17,9 +19,15 @@ public class Pitching extends Activity {
 	// CONTROLS
 	private Button _strike, _ball, _foulBall, _hit;
 	private ListView _outputList;
-
+	private TextView _pitchCounterView;
+	
+	// OBJECTS
+	private PitchCounter _pitchCounter;
+	
+	
 	// ADAPTERS
-	private PitchArrayAdapter _pitchArrayAdapter;
+	//TODO: Move this into a PitchCounter object
+	//private PitchArrayAdapter _pitchArrayAdapter;
 
 	// PUBLIC METHODS
 	@Override
@@ -36,45 +44,56 @@ public class Pitching extends Activity {
 
 	// INITIALIZATION METHODS
 	private void setup() {
-		InitializeArrayAdapters();
-		InitializeButtons();
+		this.InitializeArrayAdapters();
+		this.InitializeButtons();
+		
+		this.InitializePitchCounter();
 	}
 
 	private void InitializeButtons() {
 		this._strike = (Button) findViewById(R.id.strike);
 		this._strike.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				addPitch(true);
+				_pitchCounter.addStrike();
+				RefreshPitchCountView();
 			}
 		});
 
 		this._ball = (Button) findViewById(R.id.ball);
 		this._ball.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				addPitch(false);
+				_pitchCounter.addBall();
+				RefreshPitchCountView();
 			}
 		});
 
 		this._foulBall = (Button) findViewById(R.id.foulBall);
-		this._foulBall.setClickable(false);
-		this._foulBall.setEnabled(false);
+		this._foulBall.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				_pitchCounter.addFoulBall();
+				RefreshPitchCountView();
+			}
+		});
 
 		this._hit = (Button) findViewById(R.id.hit);
 		this._hit.setClickable(false);
 		this._hit.setEnabled(false);
 	}
 
+	private void InitializePitchCounter(){
+		this._pitchCounter = new PitchCounter();
+		this._pitchCounterView = (TextView) findViewById(R.id.pitchCounter);
+	}
+	
+	private void RefreshPitchCountView() {
+		this._pitchCounterView.setText(this._pitchCounter.getPitchCount());
+	}
+	
 	private void InitializeArrayAdapters() {
-		this._pitchArrayAdapter = new PitchArrayAdapter(this,
-				R.layout.output_template, new ArrayList<IPitch>());
-		this._outputList = (ListView) findViewById(R.id.outputList);
-		this._outputList.setAdapter(this._pitchArrayAdapter);
+//		this._pitchArrayAdapter = new PitchArrayAdapter(this,
+//				R.layout.output_template, new ArrayList<IPitch>());
+//		this._outputList = (ListView) findViewById(R.id.outputList);
+//		this._outputList.setAdapter(this._pitchArrayAdapter);
 	}
 
-	// ADD PITCH METHOD
-	private void addPitch(boolean isStrike) {
-		ICoordinate coordinate = new PitchLocation(0, 0);
-		IPitch Pitch = new Pitch(coordinate, isStrike);
-		this._pitchArrayAdapter.add(Pitch);
-	}
 }
